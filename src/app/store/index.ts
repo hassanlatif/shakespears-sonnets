6007d1c9437a44de974e23ad592e16270cb95ca0
Hassan Latif <hassan.latif@gmail.com>
2018-10-15 06:57:33 +1100

    Added localStorage sync

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -6,15 +6,17 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import { localStorageSync } from 'ngrx-store-localstorage';
 
 
 export interface AppState {
-
 }
-
 
 export const reducers: ActionReducerMap<AppState> = {
 };
 
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({keys: ['sonnets']})(reducer);
+}
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [localStorageSyncReducer] : [localStorageSyncReducer];
