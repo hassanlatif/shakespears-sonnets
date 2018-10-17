5f1b71aa69aa42314593237586592efa5a912fb6
Hassan Mughal <hmughal@noggin.com.au>
2018-10-18 10:21:50 +1100

    Swapped LocalStorageSync with Meta-reducer

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -7,6 +7,7 @@ import {
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 import { localStorageSync } from 'ngrx-store-localstorage';
+import { SonnetActionTypes } from './sonnets/sonnets.actions';
 
 
 export interface AppState {
@@ -15,8 +16,17 @@ export interface AppState {
 export const reducers: ActionReducerMap<AppState> = {
 };
 
-export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
-  return localStorageSync({keys: ['sonnets']})(reducer);
+export function saveToLocalStorage( reducer ) {
+  return function newReducer( state, action ) {
+    const nextState = reducer(state, action);
+
+    if( action.type === SonnetActionTypes.ALL_SONNETS_LOADED 
+       || SonnetActionTypes.SAVE_SONNET_REQUESTED) {
+        localStorage.setItem('state', JSON.stringify(nextState));
+    }
+
+    return nextState;
+  }
 }
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [localStorageSyncReducer] : [localStorageSyncReducer];
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [saveToLocalStorage] : [saveToLocalStorage];
